854def712485dd7c4a8a0d163186c585b5593fd3
unknown <abhayagnihotri1585@gmail.com>
2024-06-08 20:54:01 +0530

    completed-currency-converter

--- CurrencyConverter/src/CustomHooks/CurrencyInfo.js
+++ CurrencyConverter/src/CustomHooks/CurrencyInfo.js
@@ -1,17 +1,18 @@
-import { useState, useEffect } from 'react';
+import {useEffect, useState} from "react"
 
-function useCurrencyInfo(currency) {
-  const [data, setData] = useState({});
 
-  useEffect(() => {
-    fetch(`Api-Call`)
-      .then(res => res.json())
-      .then(res => setData(res[currency]))
-      .catch(error => console.error('Error fetching data:', error));
+function useCurrencyInfo(currency){
+    const [data, setData] = useState({})
+    let url = `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
 
-  }, [currency]);
-
-  return data;
+    useEffect(() => {
+        fetch(url)
+        .then((res) => res.json())
+        .then((res) => setData(res[currency]))
+        console.log(data);
+    }, [currency])
+    console.log(data);
+    return data
 }
 
 export default useCurrencyInfo;
